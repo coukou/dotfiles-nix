35f2917af8c17de1d191fefbbe721e9d8d73f6dd
coukou <orenkay57@gmail.com>
2023-12-06 23:43:11 +0100

    feat(nvim): plugins bootstrap

--- home-manager/apps/nvim/default.nix
+++ home-manager/apps/nvim/default.nix
@@ -7,11 +7,52 @@ in
 {
   options.apps.nvim = { enable = lib.mkEnableOption "nvim"; };
   config = lib.mkIf cfg.enable {
-    programs.neovim = {
-      enable = true; 
+    programs.neovim = 
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
+      enable = true;
+      vimAlias = true;
+
+      extraPackages = with pkgs; [
+        lua-language-server
+        rnix-lsp
+
+        xclip
+        wl-clipboard
+      ];
+
+
       plugins = with pkgs.vimPlugins; [
+
+        luasnip
+
+        # cmp
+        cmp_luasnip
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./lua/cmp.lua;
+        }
+
+        # LSP
+        cmp-nvim-lsp
+        {
+          plugin = nvim-lspconfig;
+          config = toLuaFile ./lua/lsp.lua;
+        }
+
+        # Treesitter
+        {
+          plugin = nvim-treesitter.withAllGrammars;
+          config = toLuaFile ./lua/treesitter.lua;
+        }
+
+        # Others
+        neodev-nvim
+        nvim-web-devicons
         vim-nix
-	nvim-treesitter.withAllGrammars
       ];
     };
   };
