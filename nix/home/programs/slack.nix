2a3f86b05c355c08c20c38e73faa88a15d170f52
coukou <orenkay57@gmail.com>
2024-03-21 01:07:00 +0100

    patch slack for screen sharing

--- nix/home/programs/slack.nix
+++ nix/home/programs/slack.nix
@@ -1,5 +1,18 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
+{ pkgs, ... }:
+let
+  # Slack with screen sharing patch
+  # https://github.com/flathub/com.slack.Slack/issues/101#issuecomment-1927729514
+  slack = pkgs.slack.overrideAttrs (previousAttrs: {
+    installPhase =
+      previousAttrs.installPhase
+      + ''
+        sed -i'.backup' -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+
+      '';
+  });
+in
+{
+  home.packages = [
     slack
   ];
 }
