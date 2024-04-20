80e167b03e23c15db8e068d41b39ea679361cbf1
coukou <orenkay57@gmail.com>
2024-04-20 13:47:40 +0200

    use nvidia discrete gpu with patched driver

--- nix/machines/desktop.nix
+++ nix/machines/desktop.nix
@@ -13,35 +13,30 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
 
+
   hardware.nvidia.package =
+    let
+      rcu_patch = pkgs.fetchpatch {
+        url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+        hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+      };
+    in
     config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "550.54.14";
-      sha256_64bit = "sha256-jEl/8c/HwxD7h1FJvDD6pP0m0iN7LLps0uiweAFXz+M=";
-      openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-      settingsSha256 = "sha256-m2rNASJp0i0Ez2OuqL+JpgEF0Yd8sYVCyrOoo/ln2a4=";
-      persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-    };
+      version = "535.154.05";
+      sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+      sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+      openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+      settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+      persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
+      patches = [ rcu_patch ];
     };
-
-    nvidiaBusId = "PCI:1:0:0";
-    amdgpuBusId = "PCI:10:0:0";
-  };
 
   environment.systemPackages = with pkgs; [ ];
 
   hardware.bluetooth.enable = true;
 
   environment.sessionVariables = {
-    # Card0 : AMD iGPU
-    # Card1 : NVIDIA RTX 4070 SUPER
-    # Set priority to AMD iGPU for wayland sessions
-    WLR_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
-
     WLR_NO_HARDWARE_CURSORS = "1";
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
