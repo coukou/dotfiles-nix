5dbd41a05066f49915137761212333718e143965
coukou <orenkay57@gmail.com>
2024-07-15 20:34:40 +0200

    fix cmp

--- nixvim/config/cmp.nix
+++ nixvim/config/cmp.nix
@@ -25,8 +25,6 @@
           cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -36,8 +34,6 @@
           cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
