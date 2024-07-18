b06fa34cf7356020520f462cb881f86965c7ea5f
coukou <orenkay57@gmail.com>
2024-07-18 11:08:56 +0200

    update kitty conf

--- nix/home/programs/kitty.nix
+++ nix/home/programs/kitty.nix
@@ -5,8 +5,8 @@
     extraConfig = ''
       font_family FiraCode Nerd Font
 
-      window_padding_width 8
-      background_opacity 0.8
+      window_padding_width 12
+      background_opacity 0.62
       backgroud_blur 1
       cursor none
     '';
