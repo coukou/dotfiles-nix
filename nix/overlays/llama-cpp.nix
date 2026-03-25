b1a3837a05d771703385d5e1a12b0aa57a454dfd
coukou <orenkay57@gmail.com>
2026-03-25 15:01:59 +0100

    update llama-cpp

--- nix/overlays/llama-cpp.nix
+++ nix/overlays/llama-cpp.nix
@@ -5,27 +5,24 @@
       cudaSupport = true;
       rocmSupport = false;
       metalSupport = false;
-      # Enable BLAS for optimized CPU layer performance (OpenBLAS)
       blasSupport = true;
     }).overrideAttrs
       (oldAttrs: rec {
-        version = "8215";
+        version = "8514";
         src = final.fetchFromGitHub {
           owner = "ggml-org";
           repo = "llama.cpp";
           tag = "b${version}";
-          hash = "sha256-hBY2E3aJNnBJ5WjFrIjgpri6/7l6h2i6UI28V55fohg=";
+          hash = "sha256-V7lISYIqimGgQaivdJSK+j/BwH4bqTCk7sJ+8pF8W64=";
           leaveDotGit = true;
           postFetch = ''
             git -C "$out" rev-parse --short HEAD > $out/COMMIT
             find "$out" -name .git -print0 | xargs -0 rm -rf
           '';
         };
-        # Enable native CPU optimizations (AVX, AVX2, etc.)
         cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
           "-DGGML_NATIVE=ON"
         ];
-        # Disable Nix's march=native stripping
         preConfigure = ''
           export NIX_ENFORCE_NO_NATIVE=0
           ${oldAttrs.preConfigure or ""}
