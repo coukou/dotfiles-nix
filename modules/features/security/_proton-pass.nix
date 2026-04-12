ef7934b61b35ac80f713e95c05b64352282c816a
coukou <orenkay57@gmail.com>
2026-04-12 23:42:46 +0200

    fix: explicitly set PROTON_PASS_LINUX_KEYRING in service environment

--- modules/features/security/_proton-pass.nix
+++ modules/features/security/_proton-pass.nix
@@ -18,6 +18,7 @@
       };
       Service = {
         ExecStart = "${pkgs.proton-pass-cli}/bin/pass-cli ssh-agent start --vault-name=${config.myConfig.protonPassVault}";
+        Environment = [ "PROTON_PASS_LINUX_KEYRING=dbus" ];
         Restart = "on-failure";
         RestartSec = "5";
       };
